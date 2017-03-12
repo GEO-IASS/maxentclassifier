49e469ed989d0e2ea8a6191975ef017159ac36d7
Jon Bisila <bisilaj@carleton.edu>
2017-03-12 15:12:51 -0500

    fixed styling issues

--- maxEntClassifier.py
+++ maxEntClassifier.py
@@ -12,35 +12,34 @@ import itertools
 warnings.filterwarnings('error')
 
 
-
 def load_data(filename, features):
      return preprocess.processData(filename,features)
 
 
-#Creates empiricals given the set of feature vectors, and the true labels
+# Creates empiricals given the set of feature vectors, and the true labels
 def getEmpiricals(instances, labels):
 
     F = len(instances[0])
     N = len(instances)
-    #print(F,N)
-    #Empirical1[i] will store the probability that some instance will have true
-    #label 0, and will have a 1 for feature i
+    # Empirical1[i] will store the probability that some instance will have true
+    # label 0, and will have a 1 for feature i
     empirical0 = np.zeros(F)
     empirical1 = np.zeros(F)
     for i in range(N):
-        #If the true label is 0, adds features from instance to empirical0
+        # If the true label is 0, adds features from instance to empirical0
         if labels[i] == 0:
             empirical1 = np.add(empirical0, instances[i])
         else:
             empirical2 = np.add(empirical1, instances[i])
 
-    #Normalizes empiricals
+    # Normalizes empiricals
     empirical0 /= N
     empirical1 /= N
     return (empirical1, empirical2)
 
-#Given a feature vector, and current weight vectors, returns probability
-#that instance is from class 0 or class 1
+
+# Given a feature vector, and current weight vectors, returns probability
+# that instance is from class 0 or class 1
 def getProbs(instance, w0, w1):
 
     prob0 = np.exp(np.dot(instance, w0))
@@ -48,8 +47,9 @@ def getProbs(instance, w0, w1):
     const = prob0 + prob1
     return(prob0/ const, prob1/const)
 
-#Classifies feature vectors from instances, and returns the proportion of
-#guesses that are correct
+
+# Classifies feature vectors from instances, and returns the proportion of
+# guesses that are correct
 def testTraining(instances, labels, weights0, weights1):
     correct = 0
     N = len(instances)
@@ -59,11 +59,12 @@ def testTraining(instances, labels, weights0, weights1):
         correct += int(guess == labels[i])
     return correct/N
 
-#Performs a full iteration of maxent, and updates the weights
+
+# Performs a full iteration of maxent, and updates the weights
 def update(instances, weights0, weights1, V, F, N, emp0, emp1):
 
-    #Model0, model1 are the feature distributions as per our model given the
-    #current weights
+    # Model0, model1 are the feature distributions as per our model given the
+    # current weights
     model0 = np.zeros(F)
     model1 = np.zeros(F)
     for instance in instances:
@@ -74,22 +75,22 @@ def update(instances, weights0, weights1, V, F, N, emp0, emp1):
     model1 /= N
     for i in range(F):
 
-        #If the model prediction is 0 for some feature, sets
-        #corresponding weight to 0. In some cases, this may be due
-        #to python rounding down very small numbers to 0
+        # If the model prediction is 0 for some feature, sets
+        # corresponding weight to 0. In some cases, this may be due
+        # to python rounding down very small numbers to 0
         try:
-            weights0[i] = weights0[i] * (emp0[i] / model0[i])**(1/V)
+            weights0[i] *= (emp0[i] / model0[i]) ** (1 / V)
         except:
             weights0[i] = 0
         try:
-            weights1[i] = weights1[i] * (emp1[i] / model1[i])**(1/V)
+            weights1[i] *= (emp1[i] / model1[i]) ** (1 / V)
         except:
             weights1[i] = 0
 
     return weights0, weights1
 
 
-#Creates and tests a maximum entropy model, given a list of desired features
+# Creates and tests a maximum entropy model, given a list of desired features
 def maxEnt(features):
     print(features)
     instances, labels = load_data("processData.txt", features)
@@ -103,9 +104,9 @@ def maxEnt(features):
 
     beforeTesting = testTraining(testingData, testingLabels, weights0, weights1)
 
-    #Runs 20 iterations of updating weights. We chose 20 iterations because we
-    #observed that for any choice of features, the weights would always converge
-    #essentially completely after 20 iterations
+    # Runs 20 iterations of updating weights. We chose 20 iterations because we
+    # observed that for any choice of features, the weights would always converge
+    # essentially completely after 20 iterations
     for j in range(20):
         weights0, weights1 = update(instances, weights0, weights1, V, F, N, emp0, emp1)
 
@@ -115,8 +116,8 @@ def maxEnt(features):
     return beforeTesting, afterTesting
 
 
-#Runs maxent on all possible combinations of N features, and writes results to
-#the desired output file
+# Runs maxent on all possible combinations of N features, and writes results to
+# the desired output file
 def compareN(output, N):
     allFeatures = ["age", "workclass", "education", "education-num", "marital-status", "occupation", "capital-gain",
                    "capital-loss", "sex", "hours-per-week", "race", "native-country"]
@@ -128,7 +129,8 @@ def compareN(output, N):
             before, after = maxEnt(combo)
             f.write(str(combo) + "," + str(after) + "\n")
 
-#Runs maxEnt with the desired features
+
+# Runs maxEnt with the desired features
 def main():
     if len(sys.argv) == 1:
         allFeatures = ["age", "workclass", "education", "education-num", "marital-status", "occupation", "capital-gain", "capital-loss", "race", "native-country"]
