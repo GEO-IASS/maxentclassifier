052540cbb75a877c86f7c9ba52284548152f6986
bisilaj <bisilaj@carleton.edu>
2017-03-12 13:52:18 -0500

    refactoring comparing function

--- maxEntClassifier.py
+++ maxEntClassifier.py
@@ -73,8 +73,7 @@ def update(instances, weights0, weights1, V, F, N, emp0, emp1):
 
 
 def maxEnt(features):
-
-    print("MaxEnt Features: ", features)
+    print(features)
     instances, labels = load_data("processData.txt", features)
     V = sum(instances[0])
     F = len(instances[0])
@@ -83,115 +82,31 @@ def maxEnt(features):
     weights0 = np.ones(F)
     weights1 = np.ones(F)
     testingData, testingLabels = load_data("testData.txt", features)
-    #print(testingLabels)
 
     beforeTesting = testTraining(testingData, testingLabels, weights0, weights1)
     for j in range(20):
         weights0, weights1 = update(instances, weights0, weights1, V, F, N, emp0, emp1)
-        # print(testTraining(instances, labels, weights0, weights1))
-        # print(weights0)
-        # print()
-        # print(weights1)
-        # print()
-    # print(weights0, weights1)
+
 
     afterTesting = testTraining(testingData, testingLabels, weights0, weights1)
 
     return beforeTesting, afterTesting
 
-def compareEachFeature():
+
+def compareN(output, N):
     allFeatures = ["age", "workclass", "education", "education-num", "marital-status", "occupation", "capital-gain",
                    "capital-loss", "sex", "hours-per-week"]
-
-    if os.path.isfile("compareIndividualFeatures.txt"):
-        os.remove("compareIndividualFeatures.txt")
-    with open("compareIndividualFeatures.txt", "a") as f:
-        for feature in allFeatures:
-            features = [feature]
-            before, after = maxEnt(features)
-            print("Feature: " + feature, before, after)
-            f.write(feature + " " + str(before) + " " + str(after) + "\n")
-
-def comparePairs():
-    allFeatures = ["age", "workclass", "education", "education-num", "marital-status", "occupation", "capital-gain",
-                   "capital-loss", "sex", "hours-per-week"]
-    if os.path.isfile("comparePairs.txt"):
-        os.remove("comparePairs.txt")
-    with open("comparePairs.txt", "a") as f:
-        for i in range(0, len(allFeatures)):
-            for j in range(i+1, len(allFeatures)):
-                before, after = maxEnt([allFeatures[i], allFeatures[j]])
-                f.write("Features " + allFeatures[i] + " & " + allFeatures[j] + ": " + "Before training: " + str(before)
-                        + " " + "After training: " + str(after) + "\n")
-
-
-def compareTriples():
-    allFeatures = ["age", "workclass", "education", "education-num", "marital-status", "occupation", "capital-gain",
-                   "capital-loss", "sex", "hours-per-week"]
-    combos = itertools.combinations(allFeatures, 3)
-    if os.path.isfile("compareTriples.txt"):
-        os.remove("compareTriples.txt")
-    with open("compareTriples.txt", "a") as f:
+    combos = itertools.combinations(allFeatures, N)
+    if os.path.isfile(output):
+        os.remove(output)
+    with open(output, "a") as f:
         for combo in combos:
             before, after = maxEnt(combo)
-            f.write("Features " + str(combo) + ": " + "Before training: " + str(before)
-                        + " " + "After training: " + str(after) + "\n")
-
-def compareSevens():
-    allFeatures = ["age", "workclass", "education", "education-num", "marital-status", "occupation", "capital-gain",
-                   "capital-loss", "sex", "hours-per-week"]
-    combos = itertools.combinations(allFeatures, 7)
-    if os.path.isfile("compareSevens.txt"):
-        os.remove("compareSevens.txt")
-    with open("compareSevens.txt", "a") as f:
-        for combo in combos:
-            before, after = maxEnt(combo)
-            f.write("Features " + str(combo) + ": " + "Before training: " + str(before)
-                        + " " + "After training: " + str(after) + "\n")
-
-def compareEights():
-    allFeatures = ["age", "workclass", "education", "education-num", "marital-status", "occupation", "capital-gain",
-                   "capital-loss", "sex", "hours-per-week"]
-    combos = itertools.combinations(allFeatures, 8)
-    if os.path.isfile("compareEights.txt"):
-        os.remove("compareEights.txt")
-    with open("compareEights.txt", "a") as f:
-        for combo in combos:
-            before, after = maxEnt(combo)
-            f.write("Features " + str(combo) + ": " + "Before training: " + str(before)
-                        + " " + "After training: " + str(after) + "\n")
-
-def compareNines():
-    allFeatures = ["age", "workclass", "education", "education-num", "marital-status", "occupation", "capital-gain",
-                   "capital-loss", "sex", "hours-per-week"]
-    combos = itertools.combinations(allFeatures, 9)
-    if os.path.isfile("compareNines.txt"):
-        os.remove("compareNines.txt")
-    with open("compareNines.txt", "a") as f:
-        for combo in combos:
-            before, after = maxEnt(combo)
-            f.write("Features " + str(combo) + ": " + "Before training: " + str(before)
-                        + " " + "After training: " + str(after) + "\n")
-
+            f.write(str(combo) + "," + str(after) + "\n")
 def main():
     if len(sys.argv) == 1:
-        allFeatures = ["age", "workclass", "education", "education-num", "marital-status", "occupation", "capital-gain", "capital-loss"]
-        sig_features = ["workclass", "education", "education-num", "capital-gain", "capital-loss", "occupation"]
-        # sigbefore, sigafter = maxEnt(sig_features)
-        # allbefore, allafter = maxEnt(allFeatures)
-        #
-        # print("Test Before: ", allbefore, "\n")
-        # print("Test After: ", allafter, "\n")
-        # print("Training Before: ", otherbefore, "\n")
-        # print("Training After: ", otherafter, "\n")
-        # print("Significant Features: ", sigbefore, sigafter)
-        # print("All Features: ", allbefore, allafter)
-        # compareEachFeature()
-        # comparePairs()
-        # compareTriples()
-        compareSevens()
-        compareEights()
-        compareNines()
+        for i in range(1, 10):
+            compareN("compare" + str(i) + ".txt", i)
 
 
     else:
